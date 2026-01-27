8c65cd5527c61cb062460c3884f55005700e5f48
sanjaiveeran44 <kit27.am44@gmail.com>
2026-01-27 20:17:08 +0530

    Initial Spring Boot project setup

---
+++ food-ordering-backend/src/test/java/com/company/food_ordering_backend/BigbiteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.company.food_ordering_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BigbiteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
