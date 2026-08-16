77c018785f714e858ea64da9ebd5df14e6626629
sanjaiveeran44 <kit27.am44@gmail.com>
2026-08-16 17:25:10 +0530

    Add main.tsx entry point for React mounting

---
+++ Downloads/untitled (1)/src/main.tsx
@@ -0,0 +1,13 @@
+import {StrictMode} from 'react';
+import {createRoot} from 'react-dom/client';
+import App from './App.tsx';
+import './index.css';
+
+// @ts-ignore
+window.global = window;
+
+createRoot(document.getElementById('root')!).render(
+  <StrictMode>
+    <App />
+  </StrictMode>,
+);
